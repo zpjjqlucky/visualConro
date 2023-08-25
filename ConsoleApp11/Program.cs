e7689c675e4805970d3e0d76a74eb9ad0c38a617
jjq <m17674670227@163.com>
2023-08-25 11:05:56 +0800

    add

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(0.2+0.1+0.2+0.3);
+        }
+    }
+}
